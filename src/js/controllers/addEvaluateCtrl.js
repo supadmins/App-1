148f85a856bf0e7c7e15d074e153cdbc0c4fb98c
cxl <supadmins@163.com>
2016-04-09 15:53:43 +0800

    '添加评论'

--- src/js/controllers/addEvaluateCtrl.js
+++ src/js/controllers/addEvaluateCtrl.js
@@ -1,3 +1,9 @@
 angular.module('yyzWebApp')
-    .controller('addEvaluateCtrl', ['$scope', '$http', function ($scope, $http) {
+    .controller('addEvaluateCtrl', ['$scope', '$http','order', '$stateParams',
+        function ($scope, $http,order,$stateParams) {
+
+        $scope.id=$stateParams.id;
+            $scope.score=5;
+            $scope.content="";
+            $scope.save=function(){};
     }]);
