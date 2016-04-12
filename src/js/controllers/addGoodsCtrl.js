4fb9c16358909fb46de4aa401caf3f0cdbd06761
joshua <wanghaibo20081815@163.com>
2016-04-12 18:54:03 +0800

    增加商品模块

--- src/js/controllers/addGoodsCtrl.js
+++ src/js/controllers/addGoodsCtrl.js
@@ -1,12 +1,34 @@
 angular.module('yyzWebApp')
-    .controller('addGoodsCtrl', ['$scope', '$http', 'productType', 'FileUploader',
-        function ($scope, $http, productType, FileUploader) {
-            $scope.imgs = [
-                'imgs/other/goods1.png'
-            ];
-            $scope.groupList = [];
+    .controller('addGoodsCtrl', ['$scope', '$http', 'productType',
+        function ($scope, $http, productType) {
+            var params = {
+                name: $scope.name,
+                productTypeId: $scope.typeId,
+                remark: $scope.remark,
+                productPictureBindingModelList: $scope.imgs,
+                shipment: $scope.shipment,
+                shopProductTypeIdList: $scope.types,
+                productSpecificationGroupBindingModelList: $scope.grouplist,
+                productSkuBindingModelList: $scope.skuList
+            };
 
-            $scope.uploader = new FileUploader();
+            var sku = {
+                price: '',
+                salesPrice: '',
+                stock: '',
+                productSpecificationBindingModelList: []
+            };
+
+            $scope.groupList = [];
+            $scope.skuList = [];
+            $scope.imgs = [
+                {
+                    picture: 'imgs/other/goods1.png'
+                },
+                {
+                    picture: 'imgs/other/goods1.png'
+                }
+            ];
 
             $scope.doGroup = function () {
                 $scope.view2 = true;
@@ -23,5 +45,13 @@ angular.module('yyzWebApp')
                             Name: $scope.groupName
                         });
                     })
+            };
+
+            $scope.addSpec = function () {
+                $scope.skuList.push(sku);
+            };
+
+            $scope.remove = function ($index) {
+                $scope.skuList.splice($index, 1);
             };
     }]);
